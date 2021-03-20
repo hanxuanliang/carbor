b6eb64e808b6bd2ab5451e500e1c5e66b484c1c6
hanxuanliang <hxuanliang@163.com>
2021-03-20 18:25:19 +0800

    snake move logic

--- snake/src/draw/snake.rs
+++ snake/src/draw/snake.rs
@@ -1,12 +1,16 @@
+#![allow(dead_code)]
+
 use piston_window::types::Color;
 use std::collections::LinkedList;
-use piston_window::{ Context, G2d};
+use piston_window::{Context, G2d};
 
 use crate::draw::action::draw_block;
 
 const SNAKE_COLOR: Color = [0.00, 0.80, 0.00, 1.0];
 
-enum Direction {
+// 处理方向
+#[derive(Copy, Clone, PartialEq)]
+pub enum Direction {
     Up, 
     Down,
     Left,
@@ -24,6 +28,8 @@ impl Direction {
     }
 }
 
+// 构建蛇的最小区块
+#[derive(Clone)]
 struct Block {
     x: i32,
     y: i32,
@@ -53,9 +59,85 @@ impl Snake {
         }
     }
 
-    pub fn draw(&self, ctx: Context, g: &mut G2d) {
+    pub fn draw(&self, ctx: &Context, g: &mut G2d) {
         for block in &self.body {
-            
+            draw_block(SNAKE_COLOR, block.x, block.y, ctx, g);
         }
+    }
+
+    // 返回蛇的头部
+    pub fn head_pos(&self) -> (i32, i32) {
+        let head_block = self.body.front().unwrap();
+        (head_block.x, head_block.y)
+    }
+
+    // 蛇的前进
+    pub fn move_forward(&mut self, dir: Option<Direction>) {
+        match dir {
+            Some(d) => self.direction = d,
+            None => (),
+        }
+
+        let (last_x, last_y): (i32, i32) = self.head_pos();
+        let new_block = match self.direction {
+            Direction::Up => Block {
+                x: last_x,
+                y: last_y - 1,
+            },
+            Direction::Down => Block {
+                x: last_x,
+                y: last_y + 1,
+            },
+            Direction::Left => Block {
+                x: last_x,
+                y: last_y + 1,
+            },
+            Direction::Right => Block {
+                x: last_x,
+                y: last_y + 1,
+            }
+        };
+        self.body.push_front(new_block);
+        
+        let removed_block = self.body.pop_back().unwrap();
+        self.tail = Some(removed_block);
+    }
+
+    pub fn next_head(&self, dir: Option<Direction>) -> (i32, i32) {
+        let (head_x, head_y) = self.head_pos();
+
+        let mut move_dir = self.direction;
+        match dir {
+            Some(d) => move_dir = d,
+            None => {},
+        }
+
+        match move_dir {
+            Direction::Up => (head_x, head_y - 1),
+            Direction::Down => (head_x, head_y + 1),
+            Direction::Left => (head_x - 1, head_y),
+            Direction::Right => (head_x + 1, head_y),
+        }
+    }
+
+    pub fn restore_tail(&mut self) {
+        let block = self.tail.clone().unwrap();
+        self.body.push_back(block);
+    }
+
+    pub fn overlap_tail(&self, x: i32, y: i32) -> bool {
+        let mut ch = 0;
+        for block in &self.body {
+            if x == block.x && y == block.y {
+                return true;
+            }
+
+            ch += 1;
+            if ch == self.body.len() - 1 {
+                break;
+            }
+        }
+
+        return false;
     }
 }
